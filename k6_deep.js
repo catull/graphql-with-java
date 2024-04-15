f6f61e3871eb45ea2752717275227d9acd9b767d
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-04-15 13:22:36 -0700

    Added each individual k6 script

---
+++ k6_deep.js
@@ -0,0 +1,75 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { sleep } from 'k6';
+
+export const options = {
+    thresholds: {
+	http_req_failed: [{ threshold: 'rate<0.01', abortOnFail: true }],
+	http_req_duration: ['p(99)<1000'],
+    },
+    scenarios: {
+	average_load: {
+	    executor: 'ramping-vus',
+	    stages: [
+		{ duration: '300s', target: 1 },
+		// { duration: '10s', target: 20 },
+		// { duration: '10s', target: 40 },
+		// { duration: '10s', target: 80 },
+		// { duration: '10s', target: 160 },
+		// { duration: '10s', target: 320 },
+		// { duration: '10s', target: 640 },
+		// { duration: '10s', target: 1024 },
+		// { duration: '10s', target: 2048 },
+		// { duration: '10s', target: 4096 },
+		// { duration: '10s', target: 8192 },
+	    ],
+	},
+    },
+};
+
+const query = `
+query {
+  order(limit: 1) {
+    account {
+      id
+      name
+      created_at
+      updated_at
+      orders {
+        id
+        account_id
+        created_at
+        updated_at
+        status
+        order_details {
+          id
+          order_id
+          units
+          created_at
+          updated_at
+          product {
+            id
+            name
+            price
+            created_at
+            updated_at
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+const headers = {
+    'Content-Type': 'application/json',
+};
+
+export default function() {
+    const res = http.post('http://34.94.25.86/graphql', JSON.stringify({ query: query }), {
+	headers: headers,
+    })
+    check(res, {
+	'response code was 200': (res) => res.status == 200,
+    });
+}
